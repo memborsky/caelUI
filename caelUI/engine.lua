8b038cbc975ad8b747886f7aeaec9351e142a9e1
Matt Emborsky <memborsky@gmail.com>
2011-10-13 12:44:57 -0400

    caelLib: We no longer need the scales stuff. We are also doing all the event stuff inside caelUI, so we don't need that code either.

--- caelUI/engine.lua
+++ caelUI/engine.lua
@@ -12,22 +12,36 @@ local F, P, M = unpack(select(2, ...))
 local eventFrame = CreateFrame("Frame")
 
 eventFrame:RegisterEvent("ADDON_LOADED")
+eventFrame:RegisterEvent("PLAYER_LOGOUT")
+eventFrame:RegisterEvent("UPDATE_FLOATING_CHAT_WINDOWS")
+eventFrame:RegisterEvent("PLAYER_LEAVING_WORLD")
 eventFrame:SetScript("OnEvent", function(self, event)
     if addon ~= "caelUI" then
         return
     end
 
-    if not cael_user then
-        cael_user = {}
+    if event == "ADDON_LOADED" then
+        if not cael_user then
+            cael_user = {}
+        end
+
+        if not cael_global then
+            cael_global = {}
+        end
+
+        -- Initialize our databases.
+        P.database.initialize()
+
+        -- Set our UI Scale so we can provide pixel perfection.
+        P.SetScale()
+    elseif event == "PLAYER_LEAVING_WORLD" then
+        -- XXX: Needs to be moved to event system.
+        cael_user.scale = math.floor(GetCVar("uiScale") * 100 + 0.5)/100
+    elseif event == "PLAYER_LOGOUT" then
+        -- XXX: Needs to be moved to event system.
+        cael_user.scale = math.floor(GetCVar("uiScale") * 100 + 0.5)/100
+    elseif event == "UPDATE_FLOATING_CHAT_WINDOWS" then
+        -- XXX: Needs to be moved to event system.
+        cael_user.scale = math.floor(GetCVar("uiScale") * 100 + 0.5)/100
     end
-
-    if not cael_global then
-        cael_global = {}
-    end
-
-    -- Initialize our databases.
-    P.database.initialize()
-
-    -- Set our UI Scale so we can provide pixel perfection.
-    P.SetScale()
 end)
