06b28b2850e6fc557b39e929d93734e7dfd9812b
Matt Emborsky <trumpeter2003@gmail.com>
2011-10-24 01:39:47 -0400

    Dur, I know Lua, I promise.

--- caelUI/core/is_guild_group.lua
+++ caelUI/core/is_guild_group.lua
@@ -5,12 +5,12 @@ local IS_GUILD_GROUP
 local event_frame = CreateFrame("Frame")
 
 event_frame:RegisterEvent("GUILD_PARTY_STATE_UPDATED")
-event_frame:SetScript("OnEvent", function(self, event)
+event_frame:SetScript("OnEvent", function(self, event, ...)
     local isGuildGroup = ...
     if (isGuildGroup ~= IS_GUILD_GROUP) then
         IS_GUILD_GROUP = isGuildGroup
     end
-)
+end)
 
 function private.is_guild_group ()
     return IS_GUILD_GROUP
