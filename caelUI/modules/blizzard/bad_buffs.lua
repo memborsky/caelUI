8d6de29065229d9308ca8093781d619a3fd4fda8
Matt Emborsky <memborsky@gmail.com>
2011-10-27 02:13:42 -0400

    caelUI: caelCore/modules/badBuffs.lua -> caelUI/modules/blizzard/bad_buff.lua. Added the stupid wand costume crap to bad buffs.

--- caelUI/modules/blizzard/bad_buffs.lua
+++ caelUI/modules/blizzard/bad_buffs.lua
@@ -1,18 +1,19 @@
-local _, caelCore = ...
-
---[[    Auto cancel various buffs    ]]
-
-local badbuffs = caelCore.createModule("BadBuffs")
+-- Auto cancel various buffs
+local private = unpack(select(2, ...))
 
 local badBuffsList = {
     ["Mohawked!"]			= true,
     ["Rabbit Costume"]		= true,
     ["Hand of Proection"]	= true,
     ["Jack-o'-Lanterned!"]	= true,
+    ["Skeleton Costume"]    = true,
+    ["Bat Costume"]         = true,
+    ["Wisp Costume"]        = true,
+    ["Ghost Costume"]       = true,
+    ["Pirate Costume"]      = true,
 }
 
-badbuffs:RegisterEvent("UNIT_AURA")
-badbuffs:SetScript("OnEvent", function(self, event)
+private.events:RegisterEvent("UNIT_AURA", function(self, event)
     for k, v in pairs(badBuffsList) do
         if UnitAura("player", k) then
             CancelUnitBuff("player", k)
