c8ddf47c35b6d3ce19ac92c9d4442cfd9f215cd4
Matt Emborsky <memborsky@gmail.com>
2011-10-28 18:08:07 -0400

    [caelUI] Pushing afk spin into caelUI. This also is currently working perfectly as intended for me. No taint frame issues or problems with being afk entering/exiting combat and things not being as expected.

---
+++ caelUI/modules/miscellaneous/afk_spin.lua
@@ -0,0 +1,64 @@
+local private = unpack(select(2, ...))
+
+if GetCVar("AutoClearAFK") == 0 then
+    SetCVar("AutoClearAFK", 1)
+end
+
+local dimmer = CreateFrame("Frame", nil, UIParent, "SecureHandlerStateTemplate")
+dimmer:SetAllPoints()
+RegisterStateDriver(dimmer, "visibility", "[combat] hide")
+
+dimmer.button = CreateFrame("Button", nil, dimmer, "SecureActionButtonTemplate")
+dimmer.button:SetHighlightTexture(nil)
+dimmer.button:SetPushedTexture(nil)
+dimmer.button:SetNormalTexture("Interface\\DialogFrame\\UI-DialogBox-Background")
+dimmer.button:SetAttribute("type", "macro")
+dimmer.button:SetAttribute("macrotext", "/afk")
+dimmer.button:RegisterForClicks("AnyUp")
+dimmer.button:SetAllPoints()
+
+-- Hide the frame on load.
+private.events:RegisterEvent("PLAYER_ENTERING_WORLD", function()
+    dimmer:Hide()
+end)
+
+-- Everything below here makes the magic happen for the afk spinner.
+
+
+-- This works to start and stop the screen spinning and dimmer showing when we go afk and come back.
+private.events:RegisterEvent("PLAYER_FLAGS_CHANGED", function(_, _, unit)
+    -- Quit if the unit the event fired from is not ourselves.
+    if unit ~= "player" then
+        return
+    end
+
+    -- Quit if we are in combat when we go afk. This solves so many possible taint issues with the dimmer frame.
+    if UnitIsAFK(unit) and InCombatLockdown() then
+        return
+    end
+
+    if UnitIsAFK(unit) then
+        MoveViewRightStart(0.01)
+        dimmer:Show()
+    else
+        MoveViewRightStop()
+
+        if dimmer:IsVisible() then
+            dimmer:Hide()
+        end
+    end
+end)
+
+private.events:RegisterEvent("PLAYER_REGEN_ENABLED", function()
+    if UnitIsAFK("player") then
+        MoveViewRightStart(0.01)
+        dimmer:Show()
+    end
+end)
+
+private.events:RegisterEvent("PLAYER_REGEN_DISABLED", function()
+    if UnitIsAFK("player") and dimmer:IsVisible() then
+        MoveViewRightStop()
+        dimmer:Hide()
+    end
+end)
