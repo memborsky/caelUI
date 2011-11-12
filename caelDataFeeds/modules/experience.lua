c943982f93fd743581bec39e3b64048a8ad541b8
Matt Emborsky <memborsky@gmail.com>
2011-11-12 03:00:35 -0500

    [caelDataFeeds - experience] We no longer need to check the pet experience as the pet now levels with the player. Fixes #13

--- caelDataFeeds/modules/experience.lua
+++ caelDataFeeds/modules/experience.lua
@@ -12,7 +12,6 @@ experience:SetFrameStrata("MEDIUM")
 experience.text:SetParent(Minimap)
 --experience.text:SetFrameLevel(Minimap:GetFrameLevel() + 2)
 
-experience:RegisterEvent("UNIT_PET")
 experience:RegisterEvent("UNIT_LEVEL")
 experience:RegisterEvent("UNIT_EXPERIENCE")
 experience:RegisterEvent("PLAYER_XP_UPDATE")
@@ -29,44 +28,19 @@ local OnEvent = function(retVal, self, event, ...)
         return
     end
 
-    local petXp, petMaxXp
-
     local xp = UnitXP("player")
     local maxXp = UnitXPMax("player")
     local restedXp = GetXPExhaustion()
 
-    if UnitGUID("pet") then
-        petXp, petMaxXp = GetPetExperience()
-    end
-
-    local xpString
-
-    if not petMaxXp or petMaxXp == 0 then
-        --xpString = format("|cffD7BEA5xp|r %.1f%%", ((xp/maxXp)*100))
-        xpString = format("|cffD7BEA5xp|r "..(restedXp and "|cff5073a0%.1f%%|r" or "|cffffffff%.1f%%|r"), ((xp/maxXp)*100))
-    elseif UnitLevel("player") == MAX_PLAYER_LEVEL and UnitLevel("pet") ~= MAX_PLAYER_LEVEL then
-        xpString = format("|cffd7bea5pet|r " .. ("|cffffffff%.1f%%|r"), ((petXp/petMaxXp)*100))
-    else
-        --xpString = string.format("|cffD7BEA5xp|r %.1f%% |cffD7BEA5pet|r %.0f%%", ((xp/maxXp)*100), ((petXp/petMaxXp)*100))
-        xpString = format("|cffD7BEA5xp|r "..(restedXp and "|cff5073a0%.1f%%|r " or "|cffffffff%.1f%%|r ").."|cffD7BEA5pet|r %.0f%%", ((xp/maxXp)*100), ((petXp/petMaxXp)*100))
-    end
-
     experience.text:SetFont(caelUI.media.fonts.normal, 10, "OUTLINE")
-    experience.text:SetText(xpString)
+    experience.text:SetText(format("|cffD7BEA5xp|r "..(restedXp and "|cff5073a0%.1f%%|r" or "|cffffffff%.1f%%|r"), ((xp/maxXp)*100)))
 
-    if retVal then
-        return format("|cffD7BEA5player|r %s / %s", xp, maxXp), (petMaxXp and petMaxXp > 0) and format("|cffD7BEA5pet|r %s / %s", petXp, petMaxXp) or nil
-    end
 end
 
 experience:SetScript("OnEvent", function(...) OnEvent(false, ...) end)
 
 experience:SetScript("OnEnter", function(self)
     GameTooltip:SetOwner(self, "ANCHOR_TOP", 0, pixel_scale(4))
-    local playerXp, petXp = OnEvent(true)
     GameTooltip:AddLine(playerXp)
-    if petXp then
-        GameTooltip:AddLine(petXp)
-    end
     GameTooltip:Show()
 end)
