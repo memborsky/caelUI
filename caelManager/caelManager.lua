911a0f5eadee020f27fe2cbdf7c1773ee025fa31
Matt Emborsky <memborsky@gmail.com>
2011-11-23 03:34:03 -0500

    [caelManager] The zone specific stuff is gone. Character specific is gone. Localize loaded and loading to their respective functions to reduce memory usage foot print. Bump the max player level to 85, just in time for 4.3, haha.

--- caelManager/caelManager.lua
+++ caelManager/caelManager.lua
@@ -11,30 +11,16 @@ local caelManager = CreateFrame("Frame", nil, UIParent)
         BigWigs
 --]]
 local raidBM = "DBM"
-local prevInstanceType = ""
-local zone = nil
 
 -- Local variables
-local playerNameRealm = UnitName("player") .. "-" .. GetRealmName()
 local playerClass = select(2, UnitClass("player"))
-local MAX_PLAYER_LEVEL = 80
-
--- Holds a list of the current loaded addons
-local loaded = {}
-
--- Holds what we are currently loading before we load it
-local loading = {}
+local MAX_PLAYER_LEVEL = 85
 
 local addons = {
-    --[[
-    ---------------------------------------------------------------------------
-    -=( Base Addons )=-
-    ---------------------------------------------------------------------------
-    These addons will load for every user, always.
-    --]]
-
+    ----------
+    -- Base --
+    ----------
     ["base"] = {
-        -- This is for caelUI, the rewrite to version 1.0.
         ["caelUI"]              = true,
 
         ["!recBug"]             = true,
@@ -72,13 +58,9 @@ local addons = {
         ["caelGroupCD"]         = true,
     },
 
-    --[[
-    ---------------------------------------------------------------------------
-    -=( Class-Specific Addons )=-
-    ---------------------------------------------------------------------------
-    These addons will load for the specified class, always.
-    --]]
-
+    --------------------
+    -- Class Specific --
+    --------------------
     ["HUNTER"]  = {
         ["caelMisdirection"]    = true,
     },
@@ -94,14 +76,10 @@ local addons = {
         ["GearSwitcher"]        = false,
     },
 
-    --[[
-    ---------------------------------------------------------------------------
-    -=( Task-Specific Addons )=-
-    ---------------------------------------------------------------------------
-    These addons will load if you use their set name after the slash command.
-    '/addonset raid' to load raid addons, for example.
-    --]]
 
+    -------------------
+    -- Task Specific --
+    -------------------
     ["raid"] = {
         ["caelBossWhisperer"]   = true,
         ["alDamageMeter"]       = true,
@@ -115,137 +93,22 @@ local addons = {
         ["oUF_Bell"]            = false,
     },
 
-    --
-    -- BigWigs Boss encounters
-    ["BigWigs-core"] = {
-        ["BigWigs"]             = true,
-        ["BigWigs_Core"]        = true,
-        ["BigWigs_Options"]     = true,
-        ["BigWigs_Plugins"]     = true,
-        ["BigWigs_Foreign"]     = true,
-    },
-
-    ["BigWigs-zone"] = {
-        -- Cata
-        ["Baradin Hold"]             = "BigWigs_Baradin",
-        ["Bastion of Twilight"]      = "BigWigs_Bastion",
-        ["Blackwing Descent"]        = "BigWigs_Blackwing",
-        ["Throne of the Four Winds"] = "BigWigs_Throne",
-
-        -- WoTLK
-        ["Icecrown Citadel"]         = "BigWigs_Citadel",
-        --[""] = "BigWigs_Northrend",
-    },
-
-    --
-    -- DXE Boss Encounters
-    ["DXE-core"] = {
-        ["DXE"]                 = true,
-        ["DXE_Options"]         = true,
-        ["DXE_Loader"]          = true,
-    },
-
-    ["DXE-zone"] = {
-        -- Cata
-        ["Baradin Hold"]             = "DXE_Baradin",
-        ["Bastion of Twilight"]      = "DXE_Bastion",
-        ["Blackwing Descent"]        = "DXE_Descent",
-        ["Throne of the Four Winds"] = "DXE_Throne",
-        --[""] = "DXE_Kalimdor"
-        -- WoTLK
-        ["Icecrown Citadel"]         = "DXE_Citadel",
-        ["Crusaders' Coliseum"]      = "DXE_Coliseum",
-        ["Ulduar"]                   = "DXE_Ulduar",
-        ["Naxxramas"]                = "DXE_Naxxramas",
-        --[""] = "DXE_Northrend"
-    },
-
-    --
-    -- RaidWatch2 Boss Encounters
-    ["RaidWatch-core"] = {
-        ["RaidWatch"]           = true,
-        ["RaidWatch_Core"]      = true,
-        ["RaidWatch_Options"]   = true,
-        ["RaidWatch_Plugins"]   = true,
-    },
-
-    ["RaidWatch-zone"] = {
-        -- Cata
-        ["Baradin Hold"]             = "RaidWatch_BaradinHold",
-        ["Bastion of Twilight"]      = "RaidWatch_BoT",
-        ["Blackwing Descent"]        = "RaidWatch_BWD",
-        ["Throne of the Four Winds"] = "RaidWatch_TotFW",
-        -- WoTLK
-        ["The Ruby Sanctum"]         = "RaidWatch_RubySanctum",
-        ["Ulduar"]                   = "RaidWatch_Ulduar",
-        ["Vault of Archavon"]        = "RaidWatch_VoA",
-        ["Crusaders' Coliseum"]      = "RaidWatch_Coliseum",
-        ["Icecrown Citadel"]         = "RaidWatch_Icecrown",
-        -- Classic
-        ["Molten Core"]              = "RaidWatch_MC",
-        ["Blackwing Lair"]           = "RaidWatch_BWL",
-
-        -- Cata Party instances
-        ["Blackrock Caverns"]        = "RaidWatch_PartyCataclysm",
-        ["Deadmines"]                = "RaidWatch_PartyCataclysm",
-        ["Grim Batol"]               = "RaidWatch_PartyCataclysm",
-        ["Halls of Origination"]     = "RaidWatch_PartyCataclysm",
-        ["Lost City of the Tol'vir"] = "RaidWatch_PartyCataclysm",
-        ["Shadowfang Keep"]          = "RaidWatch_PartyCataclysm",
-        ["The Stonecore"]            = "RaidWatch_PartyCataclysm",
-        ["The Vortex Pinnacle"]      = "RaidWatch_PartyCataclysm",
-        ["Throne of the Tides"]      = "RaidWatch_PartyCataclysm",
-
-        -- WoTLK Party instances
-        --[""] = "RaidWatch_PartyWOTLK",
-    },
-
-    -- Deadly Boss Mods core addon
-    ["DBM-core"] = {
-        ["DBM-Core"]            = true,
-        ["DBM-GUI"]             = true,
-    },
-
-    ["DBM-zone"] = {
-        ["Baradin Hold"]                = "DBM-BaradinHold",
-        ["Bastion of Twilight"]         = "DBM-BastionTwilight",
-        ["Blackwing Descent"]           = "DBM-BlackwingDescent",
-        ["Throne of the Four Winds"]    = "DBM-ThroneFourWinds",
-    },
-
     ["party"] = {
         ["alDamageMeter"]       = true,
         ["recThreatMeter"]      = true,
         ["RaidMobMarkerHUD"]    = true,
         ["Omen"]                = true,
         ["fAnnounce"]           = true,
-
-        -- XXX: This needs to move to zone specific
-        ["DBM-Core"]            = true,
-        ["DBM-GUI"]             = true,
-        --["DBM-WorldEvents"]     = true,
-        ["DBM-Party-Cataclysm"] = true,
-
         ["Snoopy"]              = false,
-        ["oUF_Bell"]            = false,
     },
 
     ["pvp"] = {
         ["alDamageMeter"]       = true,
-
-        -- XXX: This needs to move to zone specific
-        ["DBM-Core"]            = true,
-        ["DBM-GUI"]             = true,
-        ["DBM-WorldEvents"]     = true,
-        ["DBM-PvP"]             = true,
-        ["RaidMobMarkerHUD"]    = true,
-        
         ["Snoopy"]              = false,
-        ["oUF_Bell"]            = false,
+        ["RaidMobMarkerHUD"]    = true,
     },
 
     ["dev"] = {
-        
     },
 
     ["quest"] = {
@@ -255,22 +118,92 @@ local addons = {
         ["Omen"]                = false,
     },
 
-    ["not-max-level"] = {
-        ["DBM-Core"]            = false,
-        ["DBM-GUI"]             = false,
-        ["DBM-DBM-WorldEvents"] = false,
-        ["DBM-PvP"]             = false,
-    },
+    -----------------------------
+    -- Boss Encounter Specific --
+    -----------------------------
 
-    ------------------------
-    -- Character Specific --
-    ------------------------
-    ["Keltric-Earthen Ring"] = {
-        ["oUF_Caellian"]        = true,
-        ["oUF_Drk"]             = false,
-        ["oUF_Bell"]            = false,
-        --["oUF_Caellian_Heal"]   = true,
-    }
+    -------------
+    -- BigWigs --
+    -------------
+        ["BigWigs-core"] = {
+            ["BigWigs"]             = true,
+            ["BigWigs_Core"]        = true,
+            ["BigWigs_Options"]     = true,
+            ["BigWigs_Plugins"]     = true,
+            ["BigWigs_Foreign"]     = true,
+        },
+
+        ["BigWigs-raid"] = {
+            -- Cata
+            ["BigWigs_Baradin"]     = true,
+            ["BigWigs_Bastion"]     = true,
+            ["BigWigs_Blackwing"]   = true,
+            ["BigWigs_Throne"]      = true,
+        },
+
+    ---------
+    -- DXE --
+    ---------
+        ["DXE-core"] = {
+            ["DXE"]                 = true,
+            ["DXE_Options"]         = true,
+            ["DXE_Loader"]          = true,
+        },
+
+        ["DXE-raid"] = {
+            ["DXE_Baradin"] = true,
+            ["DXE_Bastion"] = true,
+            ["DXE_Descent"] = true,
+            ["DXE_Throne"]  = true,
+        },
+
+    ----------------
+    -- RaidWatch2 --
+    ----------------
+        ["RaidWatch-core"] = {
+            ["RaidWatch"]           = true,
+            ["RaidWatch_Core"]      = true,
+            ["RaidWatch_Options"]   = true,
+            ["RaidWatch_Plugins"]   = true,
+        },
+
+        ["RaidWatch-raid"] = {
+            ["RaidWatch_BaradinHold"]   = true,
+            ["RaidWatch_BoT"]           = true,
+            ["RaidWatch_BWD"]           = true,
+            ["RaidWatch_TotFW"]         = true,
+        },
+
+        ["RaidWatch-party"] = {
+            ["RaidWatch_PartyCataclysm"] = true,
+        },
+
+    ---------
+    -- DBM --
+    ---------
+        ["DBM-core"] = {
+            ["DBM-Core"]            = true,
+            ["DBM-GUI"]             = true,
+        },
+
+        ["DBM-raid"] = {
+            ["DBM-BaradinHold"]         = true,
+            ["DBM-BastionTwilight"]     = true,
+            ["DBM-BlackwingDescent"]    = true,
+            ["DBM-ThroneFourWinds"]     = true,
+            ["DBM-Firelands"]           = true,
+            ["DBM-DragonSoul"]          = true
+        },
+
+        ["DBM-party"] = {
+            ["DBM-Party-Cataclysm"] = true,
+        },
+
+        ["DBM-pvp"] = {
+            ["DBM-PvP"]             = true,
+            ["DBM-WorldEvents"]     = true,
+            ["DBM-BaradinHold"]         = true,
+        },
 }
 
 
@@ -301,7 +234,11 @@ local function IsIn (needle, haystack)
     return false, nil
 end
 
-local function ChangeSet(set, zone)
+local function ChangeSet(set)
+    -- Holds what addons we are going to be loading.
+    local loading = {}
+
+    -- The player's level.
     local playerLevel = UnitLevel("player")
 
     -- Enable a set of addons.
@@ -318,55 +255,25 @@ local function ChangeSet(set, zone)
     -- We are going to assume from here on out that only the addons we want enabled will be enabled.
     DisableAllAddOns(index)
 
-    -- Reset our loading list
-    loading = {}
-
     EnableSet("base")
 
     if set then
-        if zone and zone == "" then
-            zone = GetRealZoneText()
-        end
+        EnableSet(set)
 
-        if set == "raid" then
-            EnableSet(set)
-
-            if addons[raidBM .. "-zone"][zone] and addons[raidBM .. "-core"] then
-                EnableSet(raidBM .. "-core")
-
-                -- Enable zone specific raid boss mod.
-                loading[addons[raidBM .. "-zone"][zone]] = true
-            end
-        elseif set == "party" then
-            EnableSet(set)
-
-            if addons[raidBM .. "-zone"][zone] and addons[raidBM .. "-core"] then
-                EnableSet(raidBM .. "-core")
-
-                -- Enable zone specific raid boss mod.
-                loading[addons[raidBM .. "-zone"][zone]] = true
-            end
-        else
-            EnableSet(set)
+        -- Enable boss mod sets
+        if addons[raidBM .. "-" .. set] then
+            EnableSet(raidBM .. "-core")
+            EnableSet(raidBM .. "-" .. set)
         end
     end
 
-    -- We assume since we aren't max level, that the questing set needs to be loaded
+    -- We assume since we aren't max level, that the questing set needs to be enabled.
     if playerLevel < MAX_PLAYER_LEVEL then
         EnableSet("quest")
     end
 
     -- Enable playerClass specific addons
     EnableSet(playerClass)
-
-    -- Enable player specific addons if they exist
-    EnableSet(playerNameRealm)
-
-    -- After all the other sets have been enabled, lets enable the set that is for us not being max level.
-    -- This is used to let us unload/load more addons since we aren't max level.
-    if playerLevel < MAX_PLAYER_LEVEL then
-        EnableSet("not-max-level")
-    end
 
     -- Here we make all the magic happen of loading our loading set.
     for key, value in pairs(loading) do
@@ -385,15 +292,6 @@ local function CheckAddOns(name, enabled)
 
     if playerLevel < MAX_PLAYER_LEVEL then
         table.insert(sets, 2, "quest")
-
-    end
-
-    if addons[playerNameRealm] then
-        table.insert(sets, playerNameRealm)
-    end
-
-    if playerLevel < MAX_PLAYER_LEVEL then
-        table.insert(sets, "not-max-level")
     end
 
     for _, set in pairs(sets) do
@@ -410,6 +308,9 @@ local function CheckAddOns(name, enabled)
 end
 
 local function AutoSwitch(zone)
+    -- Our loaded addon list.
+    local loaded = {}
+
     -- Make sure we aren't dead or in combat before we attempt to auto switch.
     if UnitIsDeadOrGhost("player") or InCombatLockdown() then return end
 
@@ -417,7 +318,6 @@ local function AutoSwitch(zone)
 
     -- Update our listing of currently loaded addons.
     do
-        loaded = {}
         local name, enabled
 
         for index = 1, GetNumAddOns() do
@@ -442,21 +342,20 @@ local function AutoSwitch(zone)
     -- We are assuming we want to use the pvp set for arenas.
     if instanceType == "arena" then instanceType = "pvp" end
 
-    -- We check out previous instance type after we have done all our conversions just to be safe.
-    --if prevInstanceType == instanceType then return end
-    prevInstanceType = instanceType
-
-    -- XXX: Need to check raid type for boss mod addons.
     if instanceType ~= "none" then
+        -- We are in some type of instance.
+
         for key, value in pairs(addons[instanceType]) do
             if (value == true and not loaded[key]) or (value == false and loaded[key]) then
-                ChangeSet(instanceType, zone)
-                return -- This will break all for loops, as we have already changed our set, we no longer need to check more.
+                ChangeSet(instanceType)
+                return
             end
         end
+
     else
-        -- Our set list for no instance
-        local sets = {"base", playerClass, playerRealmName}
+        -- We aren't in any type of instance.
+
+        local sets = {"base", playerClass}
 
         if UnitLevel("player") < MAX_PLAYER_LEVEL then
             table.insert(sets, 2, "quest")
@@ -486,26 +385,8 @@ end
 
 caelManager:RegisterEvent("PLAYER_ENTERING_WORLD")
 caelManager:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-caelManager:SetScript("OnEvent", function(self, event, ...)
-    zone = GetRealZoneText()
-
-    if event == "PLAYER_ENTERING_WORLD" then
-        local _, instanceType = IsInInstance()
-
-        if instanceType == "raid" or instanceType == "party" then
-
-            if addons[raidBM .. "-zone"][zone] then
-                for name, enabled in pairs(addons[raidBM .. "-core"]) do
-                    if enabled then
-                        LoadAddOn(name)
-                    end
-                end
-                LoadAddOn(addons[raidBM .. "-zone"][zone])
-            end
-        end
-    end
-
-    AutoSwitch(zone)
+caelManager:SetScript("OnEvent", function()
+    AutoSwitch(GetRealZoneText())
 end)
 
 local handleSlash = function(set)
