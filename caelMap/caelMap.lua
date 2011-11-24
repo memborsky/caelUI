b96a1ce5cb40991c4f13cdb29c38a5ffc4857287
Matt Emborsky <memborsky@gmail.com>
2011-11-23 22:52:12 -0500

    [caelMap] Fix taint issues with displaying the world map while in combat.

--- caelMap/caelMap.lua
+++ caelMap/caelMap.lua
@@ -1,11 +1,13 @@
 ﻿local _, caelMap = ...
 
-caelMap.eventFrame = CreateFrame"Frame"
+caelMap.eventFrame = CreateFrame("Frame")
 
 local kill = caelUI.kill
-
+local dummy = function() return end
+local pixelScale = caelUI.config.pixel_scale
 local media = caelUI.media
-local pixel_scale = caelUI.config.pixel_scale
+
+WORLDMAP_WINDOWED_SIZE = 1
 
 local Player = WorldMapButton:CreateFontString(nil, "ARTWORK")
 Player:SetPoint("TOPLEFT", WorldMapButton, 0, 40)
@@ -17,24 +19,18 @@ Cursor:SetPoint("TOPLEFT", WorldMapButton, 0, 20)
 Cursor:SetFont(media.fonts.normal, 12)
 Cursor:SetTextColor(0.84, 0.75, 0.65)
 
-local function setupMap(self)
-    WORLDMAP_QUESTLIST_SIZE = 0.7
+local ForceSmallMap = GetCVarBool("miniWorldMap")
+if ForceSmallMap == nil then
+    SetCVar("miniWorldMap", 1)
+end
 
-    if WORLDMAP_SETTINGS.size == WORLDMAP_WINDOWED_SIZE then
-        ToggleFrame(WorldMapFrame)
-        WorldMapFrame_ToggleWindowSize()
-        ToggleFrame(WorldMapFrame)
-    end
-
-    WorldMap_ToggleSizeDown = function() end
-    WorldMap_ToggleSizeUp = function() end
-
+local SetupMap = function(self)
     WorldMapFrame.oArrow = PositionWorldMapArrowFrame
     PositionWorldMapArrowFrame = function(point, frame, anchor, x, y)
         local playerX, playerY = GetPlayerMapPosition("player")
         playerX = playerX * WorldMapDetailFrame:GetWidth()
         playerY = -playerY * WorldMapDetailFrame:GetHeight()
-        WorldMapFrame.oArrow(point, frame, anchor, playerX * WORLDMAP_QUESTLIST_SIZE, playerY * WORLDMAP_QUESTLIST_SIZE)
+        WorldMapFrame.oArrow(point, frame, anchor, playerX * WORLDMAP_WINDOWED_SIZE, playerY * WORLDMAP_WINDOWED_SIZE)
     end
 
     SetCVar("questPOI", 1)
@@ -43,73 +39,37 @@ local function setupMap(self)
     WorldMapQuestShowObjectives:SetChecked(1)
     WorldMapShowDigSites:SetChecked(1)
 
-    WORLDMAP_SETTINGS.size = WORLDMAP_QUESTLIST_SIZE
-
-    UIPanelWindows["WorldMapFrame"] = { area = "center", pushable = 9, whileDead = 1 };
-
     kill(BlackoutWorld)
-    kill(WorldMapQuestDetailScrollFrame)
-    kill(WorldMapQuestRewardScrollFrame)
-    kill(WorldMapQuestScrollFrame)
-    kill(WorldMapQuestShowObjectives)
-    kill(WorldMapShowDigSites)
+    kill(WorldMapFrameCloseButton)
+    kill(WorldMapFrameMiniBorderLeft)
+    kill(WorldMapFrameMiniBorderRight)
     kill(WorldMapFrameSizeDownButton)
     kill(WorldMapFrameSizeUpButton)
-    kill(WorldMapFrameCloseButton)
-    kill(WorldMapZoneMinimapDropDown)
-    kill(WorldMapZoomOutButton)
-    kill(WorldMapLevelDropDown)
-    kill(WorldMapFrameTitle)
-    kill(WorldMapContinentDropDown)
-    kill(WorldMapZoneDropDown)
-    kill(WorldMapLevelUpButton)
-    kill(WorldMapLevelDownButton)
+    kill(WorldMapPositioningGuide)
+    kill(WorldMapQuestShowObjectives)
+    kill(WorldMapShowDigSites)
+    kill(WorldMapTitleButton)
     kill(WorldMapTrackQuest)
 
-    WorldMapFrame:EnableKeyboard(false)
-    WorldMapFrame:EnableMouse(false)
-    WorldMapFrame.EnableKeyboard = function () end
-    WorldMapFrame.EnableMouse = function () end
-
-    WorldMap_LoadTextures = function() end
-
-    WorldMapPositioningGuide:ClearAllPoints()
-    WorldMapPositioningGuide:SetPoint("CENTER")
-    WorldMapPositioningGuide.ClearAllPoints = function() end
-    WorldMapPositioningGuide.SetPoint = function() end
-
-    WorldMapDetailFrame:SetPoint("TOPLEFT", WorldMapPositioningGuide, "TOP", -502, -69)
-
-    local function StopMessingWithMyShitBlizzard(frame)
-        frame:SetScale(WORLDMAP_QUESTLIST_SIZE)
-        frame.ClearAllPoints = function () end
-        frame.SetPoint = function () end
-        frame.SetScale = function () end
-        frame.SetWidth = function () end
-        frame.SetHeight = function () end
-        frame.SetSize = function () end
-    end
-
-    StopMessingWithMyShitBlizzard(WorldMapPositioningGuide)
-    StopMessingWithMyShitBlizzard(WorldMapDetailFrame)
-    StopMessingWithMyShitBlizzard(WorldMapFrame)
-    StopMessingWithMyShitBlizzard(WorldMapButton)
-    StopMessingWithMyShitBlizzard(WorldMapFrameAreaFrame)
-
-    WorldMapFrame:SetAttribute("UIPanelLayout-enabled", false)
-
-    WorldMapDetailFrame:SetPoint("TOPLEFT", WorldMapPositioningGuide, "TOP", -502, -69)
-
-    WorldMapFrame_SetPOIMaxBounds()
-
-    WorldMapQuestPOI_OnLeave = function()
-        WorldMapTooltip:Hide()
-    end
-
     WorldMapFrame:SetAlpha(0.75)
-    WorldMapFrame.SetAlpha = function () end
+    WorldMapFrame.SetAlpha = dummy
+
+    WorldMapDetailFrame:ClearAllPoints()
+    WorldMapDetailFrame:SetPoint("BOTTOM", caelPanel_Minimap, "TOP", 0, pixelScale(75))
+    WorldMapDetailFrame:SetFrameStrata("MEDIUM")
+
+    WorldMapFrameTitle:ClearAllPoints()
+    WorldMapFrameTitle:SetParent(WorldMapDetailFrame)
+    WorldMapFrameTitle:SetPoint("BOTTOM", WorldMapDetailFrame, "TOP", 0, pixelScale(15))
+    WorldMapFrameTitle:SetFont(media.fonts.normal, 40)
+    WorldMapFrameTitle:SetTextColor(0.84, 0.75, 0.65)
+
+    WorldMapFrameAreaLabel:SetFont(media.fonts.normal, 40)
+
+    hooksecurefunc("WorldMapQuestPOI_OnLeave", function() WorldMapTooltip:Hide() end)
 
     WorldMapDetailFrame.bg = CreateFrame("Frame", nil, WorldMapDetailFrame)
+    WorldMapDetailFrame.bg:SetScale(1 / WORLDMAP_WINDOWED_SIZE)
     WorldMapDetailFrame.bg:SetPoint("TOPLEFT", -10, 50)
     WorldMapDetailFrame.bg:SetPoint("BOTTOMRIGHT", 10, -10)
     WorldMapDetailFrame.bg:SetBackdrop({
@@ -117,19 +77,21 @@ local function setupMap(self)
         edgeFile = media.files.edge, edgeSize = 4,
         insets = {left = 3, right = 3, top = 3, bottom = 3}
     })
-    WorldMapDetailFrame.bg:SetFrameStrata("BACKGROUND")
+    WorldMapDetailFrame.bg:SetFrameStrata("MEDIUM")
     WorldMapDetailFrame.bg:SetBackdropColor(0.15, 0.15, 0.15, 1)
     WorldMapDetailFrame.bg:SetBackdropBorderColor(0, 0, 0)
+    WorldMapDetailFrame.bg:SetFrameLevel(20)
 
-    WorldMapButton.cursor_coordinates = WorldMapButton:CreateFontString(nil, "ARTWORK")
-    WorldMapButton.cursor_coordinates:SetPoint("BOTTOMLEFT", WorldMapButton, "BOTTOMLEFT", 5, 5)
-    WorldMapButton.cursor_coordinates:SetFont(media.fonts.normal, 12)
-    WorldMapButton.cursor_coordinates:SetTextColor(0.84, 0.75, 0.65)
+    WorldMapButton:SetAllPoints(WorldMapDetailFrame)
+
     WorldMapButton.timer = 0.1
 
     WorldMapButton:HookScript("OnUpdate", function(self, elapsed)
         self.timer = self.timer - elapsed
-        if self.timer > 0 then return end
+        if self.timer > 0 then
+            return
+        end
+
         self.timer = 0.1
 
         local PlayerX, PlayerY = GetPlayerMapPosition("player")
@@ -137,8 +99,8 @@ local function setupMap(self)
 
         local CenterX, CenterY = WorldMapDetailFrame:GetCenter()
         local CursorX, CursorY = GetCursorPosition()
-        CursorX = ((CursorX / WORLDMAP_QUESTLIST_SIZE) - (CenterX - (WorldMapDetailFrame:GetWidth() / 2))) / 10
-        CursorY = (((CenterY + (WorldMapDetailFrame:GetHeight() / 2)) - (CursorY / WORLDMAP_QUESTLIST_SIZE)) / WorldMapDetailFrame:GetHeight()) * 100
+        CursorX = ((CursorX / WORLDMAP_WINDOWED_SIZE) - (CenterX - (WorldMapDetailFrame:GetWidth() / 2))) / 10
+        CursorY = (((CenterY + (WorldMapDetailFrame:GetHeight() / 2)) - (CursorY / WORLDMAP_WINDOWED_SIZE)) / WorldMapDetailFrame:GetHeight()) * 100
 
         if CursorX >= 100 or CursorY >= 100 or CursorX <= 0 or CursorY <= 0 then
             Cursor:SetText("Cursor X, Y • |cffAF5050Out of bounds.|r")
@@ -146,8 +108,7 @@ local function setupMap(self)
             Cursor:SetFormattedText("Cursor X, Y • %.1f, %.1f", CursorX, CursorY)
         end
     end)
-
-    self:UnregisterEvent"PLAYER_ENTERING_WORLD"
+    self:UnregisterEvent("PLAYER_ENTERING_WORLD")
 end
 
 local function fixMapIcon(unit, size)
@@ -158,55 +119,40 @@ local function fixMapIcon(unit, size)
     frame:SetHeight(size)
 end
 
-caelMap.eventFrame:RegisterEvent"WORLD_MAP_UPDATE"
-caelMap.eventFrame:RegisterEvent"PLAYER_ENTERING_WORLD"
-caelMap.eventFrame:RegisterEvent"PLAYER_REGEN_ENABLED"
-caelMap.eventFrame:RegisterEvent"PLAYER_REGEN_DISABLED"
-caelMap.eventFrame:SetScript("OnEvent", function(self, event, ...)
+caelMap.eventFrame:SetScript("OnEvent", function(self, event)
     if event == "PLAYER_ENTERING_WORLD" then
-        setupMap(self)
+        SetupMap(self)
         setupMap = nil
 
-        -- Scale the player icons on the map to be a little bigger then default width and height
         for index = 1, 4 do
-            fixMapIcon(format("WorldMapParty%d", index), pixel_scale(24))
+            fixMapIcon(format("WorldMapParty%d", index), pixelScale(24))
             if BattlefieldMinimap then
-                fixMapIcon(format("BattlefieldMinimapParty%d", index), pixel_scale(24))
+                fixMapIcon(format("BattlefieldMinimapParty%d", index), pixelScale(24))
             end
         end
         for index = 1, 40 do
-            fixMapIcon(format("WorldMapRaid%d", index), pixel_scale(24))
+            fixMapIcon(format("WorldMapRaid%d", index), pixelScale(24))
             if BattlefieldMinimap then
-                fixMapIcon(format("BattlefieldMinimapRaid%d", index), pixel_scale(24))
+                fixMapIcon(format("BattlefieldMinimapRaid%d", index), pixelScale(24))
             end
         end
-    elseif event == "PLAYER_REGEN_DISABLED" then
-        WorldMapArchaeologyDigSites:Hide()
-        WorldMapBlobFrame:Hide()
-        WorldMapPOIFrame:Hide()
 
-        WorldMapArchaeologyDigSites.Show = WorldMapArchaeologyDigSites.Hide
-        WorldMapBlobFrame.Show = WorldMapBlobFrame.Hide
-        WorldMapPOIFrame.Show = WorldMapPOIFrame.Hide
+        WorldMapBlobFrame.Show = dummy
+        WorldMapBlobFrame.Hide = dummy
+    elseif event == "PLAYER_REGEN_DISABLED" then
+        WorldMapBlobFrame:DrawBlob(WORLDMAP_SETTINGS.selectedQuestId, false)
+        WorldMapBlobFrame:DrawBlob(WORLDMAP_SETTINGS.selectedQuestId, true)
 
         WatchFrame_Update()
     elseif event == "PLAYER_REGEN_ENABLED" then
-        WorldMapArchaeologyDigSites.Show = WorldMapArchaeologyDigSites:Show()
-        WorldMapBlobFrame.Show = WorldMapBlobFrame:Show()
-        WorldMapPOIFrame.Show = WorldMapPOIFrame:Show()
-
-        if not InCombatLockdown() then
-            WorldMapArchaeologyDigSites:Show()
-            WorldMapBlobFrame:Show()
-            WorldMapPOIFrame:Show()
-        end
-
         WatchFrame_Update()
-    elseif event == "WORLD_MAP_UPDATE" then
-        -- Hack to hide the bar at the bottom of the screen from showing
-        -- because it is more special then the other 17 textures that make up the world map frame
-        if WorldMapFrameTexture18:IsShown() then
-            WorldMapFrameTexture18:Hide()
-        end
     end
 end)
+
+for _, event in next, {
+    "PLAYER_ENTERING_WORLD",
+    "PLAYER_REGEN_DISABLED",
+    "PLAYER_REGEN_ENABLED"
+} do
+    caelMap.eventFrame:RegisterEvent(event)
+end
