063ea530f5f5032e6b3b86eea207412c457efc98
Matt Emborsky <memborsky@gmail.com>
2011-11-27 01:29:22 -0500

    Fixing the way the ExtraActionBarFrame is placed on the screen. It seems this is a true action bar button and won't let us move it around on the screen like the power alt bar does.

--- caelUI/modules/blizzard/ExtraActionBarFrame.lua
+++ caelUI/modules/blizzard/ExtraActionBarFrame.lua
@@ -1,16 +1,4 @@
+local private = unpack(select(2, ...))
+
 ExtraActionBarFrame:SetParent(UIParent)
-ExtraActionBarFrame:SetMovable(true)
-ExtraActionBarFrame:EnableMouse(true)
-ExtraActionBarFrame:SetUserPlaced(true)
-ExtraActionBarFrame:SetClampedToScreen(true)
-ExtraActionBarFrame:RegisterForDrag("LeftButton")
-
-ExtraActionBarFrame:SetScript("OnDragStart", function(self)
-    self:StartMoving()
-end)
-
-ExtraActionBarFrame:SetScript("OnDragStop", function(self)
-    self:StopMovingOrSizing()
-end)
-
--- ExtraActionBarFrame:SetPoint("CENTER", UIParent, "CENTER")
+ExtraActionBarFrame:SetPoint("BOTTOM", caelPanel_Minimap, "TOP", 0, private.pixel_scale(100))
