cedf754c1751315a1cfc01f25b0f2514ea6590c8
Matt Emborsky <memborsky@gmail.com>
2011-12-08 15:40:48 -0500

    [caelUI - modules] The start of something great. Just a temporary placement, nothing exciting.

--- caelUI/core/modules.lua
+++ caelUI/core/modules.lua
@@ -1,1 +1,11 @@
-local private, modules = unpack(select(2, ...))
+local private = unpack(select(2, ...))
+
+local module_metatable = {
+    __index = function(_, name) CreateFrame("Frame", "caelUI_" .. name, UIParent),
+}
+
+function module_metatable.__index:CreatePanel ()
+
+end
+
+private.CreateModule = setmetatable({}, module_metatable)
