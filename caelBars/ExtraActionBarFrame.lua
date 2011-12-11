917df30d464c47829d8634fac7a79fe6232e623b
Matt Emborsky <memborsky@gmail.com>
2011-12-10 23:11:54 -0500

    Move ExtraActionBarFrame to caelBars from caelUI.

---
+++ caelBars/ExtraActionBarFrame.lua
@@ -0,0 +1,4 @@
+local PixelScale = caelUI.config.PixelScale
+
+ExtraActionBarFrame:SetParent(UIParent)
+ExtraActionBarFrame:SetPoint("BOTTOM", caelPanel_Minimap, "TOP", 0, PixelScale(100))
--- caelUI/modules/blizzard/ExtraActionBarFrame.lua
+++
@@ -1,4 +0,0 @@
-local private = unpack(select(2, ...))
-
-ExtraActionBarFrame:SetParent(UIParent)
-ExtraActionBarFrame:SetPoint("BOTTOM", caelPanel_Minimap, "TOP", 0, private.PixelScale(100))
